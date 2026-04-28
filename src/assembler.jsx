aa696dfab72d58efb3420146af1ea2dc959a00d6
MA8N8W <ma8n8w@hallgato.nje.hu>
2026-04-28 17:25:48 +0200

    Routing, CSS Changes

--- src/assembler.jsx
+++ src/assembler.jsx
@@ -4,6 +4,7 @@ import Footer from './pages/common/footer.jsx'
 import PageHome from './pages/pagehome.jsx'
 import PageJavaScript from "./pages/pagejavascript.jsx";
 import PageReact from "./pages/pagereact.jsx";
+import PageAxios from "./pages/pageaxios.jsx";
 //import PageSPA from "./pages/pagespa.jsx";
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
@@ -15,14 +16,14 @@ function PageAssembler() {
                 <Header headerText={"Web Programozás-I Előadás Házi Feladat"}/>
                 <Navigation/>
                 <Routes>
-                    <Route path="/home" element={<PageHome />} />
-                    <Route path="/fetch" element={<wipPageFetch />} />
-                    <Route path="/axios" element={<wipPageAxios />} />
-                    <Route path="/oojs" element={<wipPageOOJS />} />
-                    <Route path="/javascript" element={<PageJavaScript />} />
-                    <Route path="/react" element={<PageReact />} />
-                    <Route path="/spa" element={<wipPageSPA />} />
-                    <Route path="*" element={<Navigate to={"/"} />} />
+                    <Route path="/project2/home" element={<PageHome />} />
+                    <Route path="/project2/fetch" element={<wipPageFetch />} />
+                    <Route path="/project2/axios" element={<PageAxios />} />
+                    <Route path="/project2/oojs" element={<wipPageOOJS />} />
+                    <Route path="/project2/javascript" element={<PageJavaScript />} />
+                    <Route path="/project2/react" element={<PageReact />} />
+                    <Route path="/project2/spa" element={<wipPageSPA />} />
+                    <Route path="*" element={<Navigate to={"/project2/home"} />} />
                 </Routes>
                 <Footer/>
             </Router>
--- src/pages/common/navigation.jsx
+++ src/pages/common/navigation.jsx
@@ -3,29 +3,29 @@ import toggleMenu from "../../../public/scripts/navigationscript.jsx"
 
 function Navigation () {
     return(
-        <div className="navigation responsive" id="mainNav">
+        <div className="navigation" id="mainNav">
             <div className="wrapper">
                 <ul role="list">
                     <li>
-                        <NavLink to="/home" data-hover="Home" styleName={({ isActive }) => "active" + (isActive ? " activated" : "")}>Home</NavLink>
+                        <NavLink to="/project2/home" data-hover="Home" styleName={({ isActive }) => "active" + (isActive ? " activated" : "")}>Home</NavLink>
                     </li>
                     <li>
-                        <NavLink to='/javascript' data-hover="Javascript" styleName={({ isActive }) => "active" + (isActive ? " activated" : "")}>JavaScript</NavLink>
+                        <NavLink to='/project2/javascript' data-hover="Javascript" styleName={({ isActive }) => "active" + (isActive ? " activated" : "")}>JavaScript</NavLink>
                     </li>
                     <li>
-                        <NavLink to='/react' data-hover="React" styleName={({ isActive }) => "active" + (isActive ? " activated" : "")}>React</NavLink>
+                        <NavLink to='/project2/react' data-hover="React" styleName={({ isActive }) => "active" + (isActive ? " activated" : "")}>React</NavLink>
                     </li>
                     <li>
-                        <NavLink to='/spa' data-hover="SPA" styleName={({ isActive }) => "active" + (isActive ? " activated" : "")}>SPA</NavLink>
+                        <NavLink to='/project2/spa' data-hover="SPA" styleName={({ isActive }) => "active" + (isActive ? " activated" : "")}>SPA</NavLink>
                     </li>
                     <li>
-                        <NavLink to='/fetch' data-hover="Fetch" styleName={({ isActive }) => "active" + (isActive ? " activated" : "")}>Fetch</NavLink>
+                        <NavLink to='/project2/fetch' data-hover="Fetch" styleName={({ isActive }) => "active" + (isActive ? " activated" : "")}>Fetch</NavLink>
                     </li>
                     <li>
-                        <NavLink to='/axios' data-hover="Axios" styleName={({ isActive }) => "active" + (isActive ? " activated" : "")}>Axios</NavLink>
+                        <NavLink to='/project2/axios' data-hover="Axios" styleName={({ isActive }) => "active" + (isActive ? " activated" : "")}>Axios</NavLink>
                     </li>
                     <li>
-                        <NavLink to='/oojs' data-hover="OOJS" styleName={({ isActive }) => "active" + (isActive ? " activated" : "")}>OOJS</NavLink>
+                        <NavLink to='/project2/oojs' data-hover="OOJS" styleName={({ isActive }) => "active" + (isActive ? " activated" : "")}>OOJS</NavLink>
                     </li>
                     <li className="menuButton">
                         <a href="javascript:void(0);" className="icon" onClick={toggleMenu}>
